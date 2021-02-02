861dba6d533b15f4f969314cdef9234f7e6060b7
Linux <andrewpester70@gmail.com>
2021-02-02 14:50:24 -0800

    changed name of program

---
+++ sandpile.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class sandpile{
+    public int[][] grid = new int[23][23];
+    public static int fps = 0;
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String tmp = sc.next();
+        if (tmp.equals("--fps")) {
+            fps = sc.nextInt();
+        }
+        while(sc.hasNextInt()){
+            
+        }
+        while(true){
+            
+        }
+         
+    }
+
+    public static void addSand(int x, int y){
+
+    }
+}
